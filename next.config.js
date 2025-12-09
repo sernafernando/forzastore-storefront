47f856f78d57929bd368edb0b20172e6eb61cdd6
Fernando <sernafernando@gmail.com>
2025-12-09 17:20:34 -0300

    Initial commit: Medusa storefront

--- next.config.js
+++ next.config.js
@@ -13,6 +13,7 @@ const S3_PATHNAME = process.env.MEDUSA_CLOUD_S3_PATHNAME
  */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/tienda',
   logging: {
     fetches: {
       fullUrl: true,
